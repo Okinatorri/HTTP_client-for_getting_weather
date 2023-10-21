fef3616f6575303e8c749cbf8e87be01fe9875e9
Yaroslav <zymer464@gmail.com>
2023-10-21 21:50:40 +0300

    Update ppp.py

--- ppp.py
+++ ppp.py
@@ -1,7 +1,7 @@
 import requests
 
 def get_weather(city):
-    api_key = "6802be0f69dbe16900b8c390d1cffdb5"
+    api_key = "token"
     url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}"
     response = requests.get(url)
     data = response.json()
